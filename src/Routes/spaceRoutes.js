4f9930f0c0c5a23440591f97a7992aea8e2e6f0f
naanmohammed <naanahmed24@gmail.com>
2023-05-23 11:22:27 +0300

    Update Routes

--- src/Routes/spaceRoutes.js
+++ src/Routes/spaceRoutes.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Header from '../Components/Header';
+import MyProfile from '../Components/MyProfile';
 
 const SpaceRoutes = () => (
   <>
     <div className="container-fluid">
       <Header />
       <Routes>
-        <Route path="/" element={<h1>Test</h1>} />
+        <Route path="/" element={<h1>Rockets</h1>} />
+        <Route path="/myProfile" element={<MyProfile />} />
+        <Route path="/missions" element={<h1>Missions</h1>} />
+        <Route path="/dragons" element={<h1>Dragons</h1>} />
       </Routes>
     </div>
   </>
