f320fd090a2af6d68a93a79d5b18e0ab005fd6b7
naanmohammed <naanahmed24@gmail.com>
2023-05-23 14:56:37 +0300

    Update Rockets.jsx to render rocket list

--- src/Components/Rockets.jsx
+++ src/Components/Rockets.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import '../Styles/Rockets.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getNeededRockets } from '../Redux/rocketsSlice';
+import '../Styles/Rockets.css'
 
 function Rockets() {
+  const { rocketList } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getNeededRockets());
+  }, [dispatch]);
+
+
   return (
     <>
-      <div className="container">
-        <img
-          className="img"
-          alt="Rocket"
-        />
-        <div className="rocket_info">
-          <h2>rocket.rocket_name</h2>
-          <p>
-            (rocket.reserved or not)
-            rocket.description
-          </p>
-          <button type="submit">
-            Reserve Rocket
-          </button>
-        </div>
-      </div>
+      {rocketList.length > 0 ? (
+        rocketList.map((rocket) => (
+          <div key={rocket.id} className='container'>
+            <img className='img'
+              src={rocket.flickr_images[0]}
+              alt="Rocket"
+            />
+            <div className='rocket_info'>
+              <h2>{rocket.rocket_name}</h2>
+              <p>
+                {rocket.reserved && <span className='reserved_sign'>Reserved</span>}
+                {rocket.description}
+              </p>
+              <button
+                className='reserve'
+              >
+                Reserve Rocket
+              </button>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
