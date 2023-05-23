2f1d1c2bce443eb3ef1a4d9496d25621be29d279
FevenSeyfu <fevenseyfu00@gmail.com>
2023-05-23 19:35:08 +0300

    Dargon basic structure

--- src/Components/Dragons.jsx
+++ src/Components/Dragons.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import '../Styles/Dragons.css'
 function Dragons() {
   return (
-    <>
-      <h1>Dragons</h1>
-		</>
+    <main className="dragon-container">
+      <div className="dragon-card">
+        <img alt="Dragon" className="dragon-img" />
+				<div className="section-left">
+           <h2 className="dragon-header">
+            Dragon 1
+					</h2>
+					<p className="dragon-detail"><span className="reserved-span"></span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, ducimus? Vero, voluptatibus mollitia. Animi impedit illum adipisci voluptas ad, aliquid doloribus modi consectetur. Quas porro quam voluptatum. Eius, dolores aliquam.</p>
+				<button className="reserve-btn">Reserve Dragon</button>
+				</div>
+			</div>
+		</main>
   );
 }
 
