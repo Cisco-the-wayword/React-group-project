6b2fee933d58429de7812f1fcbef86d2a9a8a789
naanmohammed <naanahmed24@gmail.com>
2023-05-23 13:17:41 +0300

    Update rocketsSlice to fetch data from api

---
+++ src/Redux/rocketsSlice.jsx
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const url = 'https://api.spacexdata.com/v3/';
+const endPoint = 'rockets';
+
+export const getRockets = createAsyncThunk('rockets/getRockets', async () => {
+  const response = await fetch(`${url}${endPoint}`);
+  const rockets = await response.json();
+  return rockets;
+});
+
+const initialState = {
+  rocketList: [],
+};
+
+const rocketSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {
+    reservation: (state, action) => {
+      const rocketId = action.payload;
+      state.rocketList = state.rocketList.map((rocket) =>
+        rocket.id === rocketId
+          ? { ...rocket, reserved: !rocket.reserved }
+          : rocket
+      );
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRockets.pending, (state) => {
+        state.rocketList = [];
+      })
+      .addCase(getRockets.fulfilled, (state, action) => {
+        state.rocketList = action.payload;
+      });
+  },
+});
+
+export const { reservation } = rocketSlice.actions;
+
+export default rocketSlice.reducer;
+
+export const getNeededRockets = () => async (dispatch, getState) => {
+  const { rocketList } = getState().rockets;
+
+  if (rocketList.length === 0) {
+    await dispatch(getRockets());
+  }
+};
