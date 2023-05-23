4df878b3d8b589804b5dbc98efa0ea631331d013
naanmohammed <naanahmed24@gmail.com>
2023-05-23 14:21:30 +0300

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import missionReducer from './missionSlice';
 import rocketReducer from './rocketsSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketReducer,
+    missions: missionReducer,
   },
 });
 
